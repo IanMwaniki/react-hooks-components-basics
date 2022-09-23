fb45e0f262e1e6385a025680e890da691ec08744
Ian Maina Mwaniki <ian.mwaniki@student.moringaschool.com>
2022-09-24 01:53:04 +0300

    Component basics understood

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from "react"; 
 import Article from "./Article";
 import Comment from "./Comment";
 
-function App() {
-  return (
-    <div>
-      <Article />
-      <Comment />
-    </div>
-  );
+function App(){
+    return(
+        <div>
+        <Article />
+        <Comment />
+        </div>
+    );
 }
 
 export default App;
